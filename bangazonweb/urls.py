aa9487b9871459371bb9c3b0182007d04335dd63
Sebastian Civarolo <scivarolo@gmail.com>
2019-02-07 11:25:16 -0600

    change urls to path

--- bangazonweb/urls.py
+++ bangazonweb/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import path, include
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('website.urls')),
+    path("admin/", admin.site.urls),
+    path("website/", include('website.urls')),
 ]

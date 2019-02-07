c258ebde3215ac0e55d47c37f8f3376d4aa4ad9e
Sebastian Civarolo <scivarolo@gmail.com>
2019-02-07 09:39:12 -0600

    build models

--- bangazonweb/settings.py
+++ bangazonweb/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
+    'safedelete'
 ]
 
 MIDDLEWARE = [
@@ -112,7 +113,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
--- website/models.py
+++ website/models.py
@@ -1,13 +1,93 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    """Defines a model for a customer, including userId, address, and phone number
+
+        Author: Brendan McCray
+        Returns: __str__ userId, address, and phone number
+
+    """
+
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    address = models.CharField(max_length=255)
+    phone_number = models.IntegerField()
+    delete_date = models.DateTimeField(default=None)
+
+    def __str__(self):
+        return f"User: {self.user} Address:{self.address} Phone: {self.phone_number}"
+
+
+class ProductType(models.Model):
+    """ Defines a product type.
+
+        Author: Sebastian Civarolo
+
+    """
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
-    seller = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-    )
+    """Defines a Product
+
+        Author: God
+    """
+    seller = models.ForeignKey(User, on_delete=models.PROTECT)
+    product_type = models.ForeignKey(ProductType, on_delete=models.PROTECT)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.IntegerField()
     quantity = models.IntegerField()
+    delete_date = models.DateTimeField(default=None)
+
+    def __str__(self):
+        return f"Title: {self.title} Description:{self.description} Price:{self.price} Qty:{self.quantity}"
+
+class PaymentType(models.Model):
+    """Defines a payment type.
+
+
+        Author:Jase Hackman
+
+        Returns: PaymentType Name
+    """
+    name = models.CharField(max_length = 50)
+    accountNumber = models.IntegerField()
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    delete_date = models.DateTimeField(default=None)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    """ Defines an order
+
+        Author: Rachel Daniel
+        Methods: __str__ returns full name and completed (bool)
+    """
+
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    payment_type = models.ForeignKey(PaymentType, on_delete=models.PROTECT, default=None, null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Full Name: {self.customer.first_name} {self.customer.last_name} Completed:{self.is_completed}"
+
+class OrderProduct(models.Model):
+    """Defines the join table model for a product that is assigned to an order
+
+        Author: Brendan McCray
+        Returns: __str__ productId and orderId
+
+    """
+    # cascade used here because open orders are hard deleted, so we want to remove join tables also
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Product: {self.product} Order:{self.order}"
+

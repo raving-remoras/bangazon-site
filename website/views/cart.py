96359c58f835e6fd466aedd8c6081d569d3c6875
Brendan McCray <rbmccr@gmail.com>
2019-02-07 15:53:41 -0600

    cart loads username and order number

--- website/views/cart.py
+++ website/views/cart.py
@@ -7,35 +7,16 @@ from django.contrib import messages
 from website.models import *
 
 @login_required
-def view_open_order(request):
+def cart(request):
     if request.method == "GET":
-        user_id = request.user.id
+        user_id = request.user.customer.id
+
         sql = """SELECT *
         FROM website_order
-        WHERE auth_user = %s AND website_order.payment_type_id IS NOT NULL
+        WHERE customer_id = %s AND website_order.payment_type_id IS NULL
         """
         order = Order.objects.raw(sql, [user_id])[0]
+        items = None
 
-        context = {"order": order}
-        return render(request, 'cart.html', context)
-
-# def add_brand(request):
-#     '''View for adding bike brands
-#     Allowed verbs: GET, POST
-#     returns form to post new bike brands and redirects users to a link to list of bike brands
-#     '''
-#     if request.method == "GET":
-#         #render the form page
-#         brand_form = BrandForm()
-#         return render(request, 'brands/create.html', {"brand_form": brand_form})
-
-#     if request.method == "POST":
-#         form_data = request.POST
-
-#         newBrand = Brand(
-#             name = form_data['name'],
-#             location = form_data['location'],
-#         )
-#         newBrand.save()
-#         messages.success(request, 'Saved!')
-#         return HttpResponseRedirect(reverse("bikes:brand_list"))
+        context = {"order": order, "items": items}
+        return render(request, "cart.html", context)

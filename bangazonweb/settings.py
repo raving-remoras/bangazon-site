07c7c10f9979826cf7e0faa13404b075f23e4e2b
Sebastian Civarolo <scivarolo@gmail.com>
2019-02-08 09:32:18 -0600

    install crispy forms

--- bangazonweb/settings.py
+++ bangazonweb/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'website'
+    'website',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'

b6fc4aa48259aeb10361c0abfe050d75c758b758
kellymorin <kmorin06@gmail.com>
2019-02-08 15:14:38 -0600

    Working add to cart functionality.

--- website/urls.py
+++ website/urls.py
@@ -13,5 +13,7 @@ urlpatterns = [
     # ex. /website/products
     path("products/", views.list_products, name='products'),
     # ex. /website/products/5
-    path("products/<int:product_id>", views.product_details, name="product_details")
+    path("products/<int:product_id>", views.product_details, name="product_details"),
+    # ex. /website/products/5/add
+    path("products/<int:product_id>/add", views.add_to_cart, name="add_to_cart")
 ]
--- website/views/product_views.py
+++ website/views/product_views.py
@@ -1,3 +1,9 @@
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.db import connection
+from django.urls import reverse
 from django.shortcuts import get_object_or_404, render
 from website.models import Product, OrderProduct, Order
 
@@ -6,15 +12,22 @@ def list_products(request):
         all_products = Product.objects.raw(f"""
             SELECT * FROM website_product
         """)
+        context = {
+            "products": all_products
+        }
+
     else:
         user_id = request.user.customer.id
         all_products = Product.objects.raw(f"""
             SELECT * FROM website_product
             WHERE website_product.seller_id IS NOT {user_id}
         """)
+        context = {
+            "products": all_products
+        }
 
     template_name = 'product_list.html'
-    return render(request, template_name, {'products': all_products})
+    return render(request, template_name, context)
 
 def product_details(request, product_id):
     product_details = Product.objects.raw(f"""
@@ -54,3 +67,95 @@ def product_details(request, product_id):
         "cart_count": cart_count
     }
     return render(request, "product_detail.html", context)
+
+def add_to_cart(request, product_id):
+    if not request.user.is_authenticated:
+        product_details = Product.objects.raw(f"""
+            SELECT * FROM website_product
+            WHERE website_product.id == {product_id}
+        """)[0]
+
+        purchased_qty = OrderProduct.objects.raw(f"""
+            SELECT * FROM website_orderproduct
+            LEFT JOIN website_order ON website_order.id = website_orderproduct.order_id
+            WHERE website_order.payment_type_id IS NOT null
+            AND website_orderproduct.product_id = {product_id}
+        """)
+
+        cart_qty = OrderProduct.objects.raw(f"""
+            SELECT * FROM website_orderproduct
+            LEFT JOIN website_order ON website_order.id = website_orderproduct.order_id
+            WHERE website_order.payment_type_id IS null
+            AND website_orderproduct.product_id = {product_id}
+        """)
+
+        purchased_count = 0
+        for item in purchased_qty:
+            if item.product_id == product_id:
+                purchased_count += 1
+
+        cart_count = 0
+        for item in cart_qty:
+            if item.product_id == product_id:
+                cart_count +=1
+
+        available_qty = product_details.quantity - purchased_count
+
+        context = {
+            "product_details": product_details,
+            "quantity": available_qty,
+            "cart_count": cart_count,
+            "error_message": "Please login to continue"
+        }
+        return render(request, "product_detail.html", context)
+    else:
+        try:
+            open_order = Order.objects.raw(f"""
+                SELECT * FROM website_order
+                WHERE website_order.customer_id == {request.user.customer.id}
+                AND website_order.payment_type_id IS null
+            """)[0]
+            order_id = open_order.id
+            product_id = product_id
+            data = [order_id, product_id]
+            with connection.cursor() as cursor:
+                cursor.execute("""
+                    INSERT INTO website_orderproduct
+                    (
+                        order_id,
+                        product_id
+                    )
+                    VALUES(
+                        %s, %s
+                    )
+                """, data)
+
+            return HttpResponseRedirect(reverse('website:products'))
+            # TODO: Need to figure out how to add success message
+        except:
+            customer = request.user.customer.id
+            data = [customer]
+            with connection.cursor() as cursor:
+                cursor.execute("""
+                    INSERT INTO website_order
+                    (
+                        customer_id
+                    )
+                    VALUES(
+                        %s
+                    )
+                """, data)
+                new_order = cursor.lastrowid
+                product_id = product_id
+                relationship_data = [new_order, product_id]
+                cursor.execute("""
+                    INSERT INTO website_orderproduct
+                    (
+                        order_id,
+                        product_id
+                    )
+                    VALUES(
+                        %s, %s
+                    )
+                """, relationship_data)
+            return HttpResponseRedirect(reverse('website:products'))

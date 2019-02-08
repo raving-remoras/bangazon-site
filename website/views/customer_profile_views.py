311d37cb6bd07af0d8fdb38c72d81115e35ff93b
jasehackman <jasehackman@gmail.com>
2019-02-08 11:58:22 -0600

    View now filters based on type of post. Need to add db post next

--- website/views/customer_profile_views.py
+++ website/views/customer_profile_views.py
@@ -13,16 +13,26 @@ from website.models import Product
 
 @login_required(login_url="/website/login")
 def customer_profile(request):
+    print("post1", request.POST)
     if request.method == "GET":
         user = request.user
         context = {"user": request.user}
         return render(request, 'customer_profile.html', context)
-    elif request.method == "POST" and request.POST['edit']:
-        print('edit')
-        user = request.user
-        form = {"formA": UserCustomerFormA(instance = user), "formB": UserCustomerFormB(instance = user.customer)}
-        context = {"user": request.user,
-                    "form": form}
-        return render(request, 'customer_profile.html', context)
+    elif request.method == "POST":
+        if 'edit' in request.POST:
+            user = request.user
+            form = {"formA": UserCustomerFormA(instance = user), "formB": UserCustomerFormB(instance = user.customer)}
+            context = {"user": request.user,
+                        "form": form}
+            return render(request, 'customer_profile.html', context)
+        else:
+
+
+            user = request.user
+            context = {"user": request.user}
+            return render(request, 'customer_profile.html', context)
+            print("ERROR")
+
+
 
 

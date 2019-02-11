f4618022fea785c09dffc8a65b9e6efb88c097ed
jasehackman <jasehackman@gmail.com>
2019-02-11 14:30:18 -0600

    finished test and documentation

--- website/views/closed_order_views.py
+++ website/views/closed_order_views.py
@@ -6,6 +6,13 @@ from django.db import connection
 
 @login_required(login_url="/website/login")
 def closed_order(request):
+    """Renders a page using an order id to display a closed order.
+
+                Model: Order, Product, Order_Product, PaymentType
+
+                Template: closed_order.html
+
+                Author: Jase Hackman"""
     order_id = request.POST["order_id"]
     order = Order.objects.raw("Select * From website_order WHERE id=%s", [order_id])
     order_products = OrderProduct.objects.raw("Select * FROM website_orderproduct op Join website_product p on op.product_id=p.id WHERE order_id=%s", [order_id])

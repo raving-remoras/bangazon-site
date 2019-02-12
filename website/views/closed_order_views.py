f87ac337478f2bd30fa5315cdc639511d7c8a73d
Sebastian Civarolo <scivarolo@gmail.com>
2019-02-12 14:20:18 -0600

    save and display rating to user on their order page

--- website/views/closed_order_views.py
+++ website/views/closed_order_views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.db import connection
 from website.forms import *
 from django.db import connection
 
@@ -14,13 +15,16 @@ def closed_order(request):
 
         Author: Jase Hackman
     """
-    print("POST REQUEST", request.POST)
+    if "rating" in request.POST:
+        save_product_rating(request)
+
     order_id = request.POST["order_id"]
     order = Order.objects.raw("Select * From website_order WHERE id=%s", [order_id])
     order_products = OrderProduct.objects.raw("Select * FROM website_orderproduct op Join website_product p on op.product_id=p.id WHERE order_id=%s", [order_id])
     total = 0
     for product in order_products:
         total += product.price
+
     context = {
         "order": order[0],
         "order_products": order_products,
@@ -29,4 +33,22 @@ def closed_order(request):
 
     return render(request, 'closed_order.html', context)
 
+
+def save_product_rating(request):
+    """ Called in closed_order to save the user's product rating to the database.
+
+        Author: Sebastian Civarolo
+    """
+    data = request.POST
+    rating = data['rating']
+    orderproduct = data['orderproduct_id']
+    params = [rating, orderproduct]
+    sql = """
+        UPDATE website_orderproduct
+        SET rating = %s
+        WHERE id = %s
+    """
+
+    with connection.cursor() as cursor:
+        cursor.execute(sql, params)
 

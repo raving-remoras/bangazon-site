1fa04328ee540aa2e27c7b5b36586603b1d3510c
kellymorin <kmorin06@gmail.com>
2019-02-15 09:53:45 -0600

    Updated test file for payment add

--- website/tests/test_payment_add.py
+++ website/tests/test_payment_add.py
@@ -5,12 +5,54 @@ from ..models import PaymentType, User, Customer
 
 class PaymentMethodTest(TestCase):
     """Defines tests for Payment Method model and view
+        Model:
+            PaymentType
+            User
+            Customer
 
-    Author: Rachel Daniel
-    Methods:
-        test_show_form
-        test_add_payment
+        Templates:
+            payment_form.html
+            customer_profile.html
+
+        Views:
+            payment_views.py -> add_payment
+
+        Methods:
+            setUpClass
+            test_show_form
+            test_add_payment
+
+
+        Author:
+            Rachel Daniel
+            refactored by Kelly Morin
     """
+
+    @classmethod
+    def setUpClass(cls):
+        """Creates instances of database objects before running each test in this class"""
+
+        super(PaymentMethodTest, cls).setUpClass()
+
+        # Create User
+        new_user = User.objects.create_user(
+            username="test_user",
+            first_name="Test",
+            last_name="User",
+            email="test@test.com",
+            password="secret"
+        )
+
+        # Create Customer
+        customer = Customer.objects.create(
+            street_address="123 Test LN",
+            city="Testas",
+            state="TS",
+            zipcode="11111",
+            phone_number="1111111111",
+            user=new_user
+        )
+
 
     def test_show_form(self):
         """Tests that the payment type add form page loads with expected fields if user is authenticated"""
@@ -19,25 +61,8 @@ class PaymentMethodTest(TestCase):
         response = self.client.get(reverse("website:add_payment"))
         self.assertFalse(response.content)
 
-        #create new user, log them in, and assign to customer
-        new_user= User.objects.create_user(
-            username = "testuser",
-            first_name = "Test",
-            last_name = "User",
-            email = "test@test.com",
-            password = "secret"
-            )
 
-        self.client.login(username="testuser", password="secret")
-
-        Customer.objects.create(
-            street_address = "123 Test LN",
-            city = "Testas",
-            state=  "TS",
-            zipcode = "11111",
-            phone_number = "1111111111",
-            user = new_user
-            )
+        self.client.login(username="test_user", password="secret")
 
         #test that page now loads
         response = self.client.get(reverse("website:add_payment"))
@@ -48,24 +73,7 @@ class PaymentMethodTest(TestCase):
     def test_add_payment(self):
         """Tests that the add payment form performs successful post for customer"""
 
-        new_user= User.objects.create_user(
-            username = "testuser",
-            first_name = "Test",
-            last_name = "User",
-            email = "test@test.com",
-            password = "secret"
-            )
-
-        self.client.login(username="testuser", password="secret")
-
-        Customer.objects.create(
-            street_address = "123 Test LN",
-            city = "Testas",
-            state=  "TS",
-            zipcode = "11111",
-            phone_number = "1111111111",
-            user = new_user
-            )
+        self.client.login(username="test_user", password="secret")
 
         #test submission with complete data
         response = self.client.post(reverse("website:add_payment"), {"name": "Test Payment Type", "account_number": 111111111, "customer_id": 1})
